aa24ba301dcddb146bacf3b6d244f6d6f329a837
muhammadBilal1029 <167223444+muhammadBilal1029@users.noreply.github.com>
2024-09-10 20:38:08 +0500

    updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ cartItemCount }) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
   return (
     <nav className="navbar-section">
       <Link className="navbar-brand" to="/">E-commerce</Link>
-      <ul className="navbar-links">
+      <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+      <ul className={`navbar-links ${isMobileMenuOpen ? 'open' : 'd-none'}`}>
         <li className="nav-item">
-          <Link className="nav-link" to="/">Home</Link>
+          <Link className="nav-link" to="/" onClick={closeMobileMenu}>Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/cart">
+          <Link className="nav-link" to="/cart" onClick={closeMobileMenu}>
           <div className="cart-container">
            <span className="cart-count">{cartItemCount}</span>
           Cart
